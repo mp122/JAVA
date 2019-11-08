58684bd194d4d467dbd468f7f69d40a1b488208e
Maulik47 <maulikpshah47@gmail.com>
2019-11-08 14:28:39 +0530

    sqere program

---
+++ sqrt.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.lang.Math;
+class sqrt{
+    public static void main(String args[]) {
+        System.out.println("Enter Number to find Squere : ");
+        int num = new Scanner(System.in).nextInt();
+        System.out.println("Squre of Given number : "+Math.sqrt(num));
+    }
+}
